1c98a56ee3eee8b17fec3679574956f359bca46d
Quanyong Bi <Quanyong@usc.edu>
2021-08-27 13:48:29 -0700

    Add hello_world.cpp with some code

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+//
+//  hello_world.cpp
+//  1
+//
+//  Created by 毕荃墉 on 2021/8/27.
+//
+
+#include "hello_world.hpp"
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+}
---
+++ hello_world.hpp
@@ -0,0 +1,13 @@
+//
+//  hello_world.hpp
+//  1
+//
+//  Created by 毕荃墉 on 2021/8/27.
+//
+
+#ifndef hello_world_hpp
+#define hello_world_hpp
+
+#include <stdio.h>
+
+#endif /* hello_world_hpp */
